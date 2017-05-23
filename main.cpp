bb1d5ff373abf60554eb3eeb9346e5703c5a718a
iDentitY4 <ps3udonym4@googlemail.com>
2017-05-24 00:49:04 +0200

    Testing profile

--- main.cpp
+++ main.cpp
@@ -4,23 +4,5 @@
 
 int main(int argc, char** argv) {
 
-    int childPid;
-    if((childPid = fork()) == -1)
-    {
-        fprintf(stderr, "can't fork\n");
-        _exit(1);
-    } 
-    else if (childPid == 0)
-    {
-        fprintf(stdout, "child: child pid = %d, parent pid = %d\n",
-                getpid(), getppid());
-        _exit(0);
-    }
-    else
-    {
-        fprintf(stdout, "parent: child pid = %d, parent pid = %d\n",
-                childPid, getpid());
-        _exit(0);
-    }
 }
 
