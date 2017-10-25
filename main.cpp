e7a8ee031f994add12b8c06b87429ca57fa738e5
Manuel Ketterer <ps3udonym4@googlemail.com>
2017-10-25 13:47:12 +0200

    ...

--- main.cpp
+++ main.cpp
@@ -3,39 +3,63 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <time.h>
 
 #define COMMAND_MAX_CHARS 512
 
 void read_command(char *com, char **par)
 {
     fprintf(stdout, "$ ");
-printf("test");
+    
+    //read user command
     char input [COMMAND_MAX_CHARS];
-    printf("test");
     fgets(input, COMMAND_MAX_CHARS, stdin);
 
     //remove newline
     input[strcspn(input, "\n")] = 0;
 
-printf("test");
 
     //first token is the command
-    strcpy(com, strtok(input, " "));
-
-
+    char * tok;
+    if((tok = strtok(input, " ")) == NULL)
+    {
+      return;
+    }
+    strcpy(com, tok);
+    strcpy(par[0], tok);
 
     //all following tokens are parameters for the command
-    char * tok;
-    int i = 0;
+    int i = 1;
     for(; (tok = strtok(NULL, " ")) != NULL; i++)
     {
       strcpy(par[i], tok);
     }
-    par[i] = 0;
-
-    printf("read: %s %s", com, par[0]);
+    par[i] = NULL;
 
     return;
+}
+
+void log(char* filename, char *com, char **parameters)
+{
+    FILE * f;
+    if((f = fopen(filename, "w")) == NULL)
+    {
+        fprintf(stderr, "error opening log file\n");
+        exit(2);
+    }
+
+    time_t t = time(NULL);
+    time(&t);
+    //log command
+    fprintf(f, "[%s]", ctime(&t));
+
+    for(int i = 0; i < sizeof(parameters); i++)
+    {
+        fprintf(f," %s", parameters[i]);
+    }
+    fprintf(f, "\n");
 }
 
 int main()
@@ -44,10 +68,21 @@ int main()
     int status;
     char command[20];
     char * parameters[60];
+    //int logFd;
+    //char * logFileName = "input.log";
+
+    //Create a log file for all user input
+    /*if((logFd = creat(logFileName, 0777)) < 0)
+    {
+      fprintf(stderr, "log create error\n");
+      exit(2);
+    }*/
 
     while(1)
     {
         read_command(command, parameters);
+
+        //log(logFileName, command, parameters);
 
         //flush stream before forking to avoid stream errors
         fflush(stdin);
@@ -60,7 +95,6 @@ int main()
         }
         else if(childPid == 0) /* child */
         {
-            printf("executing command: %s %s\n", command, parameters[0]);
             execvp(command, parameters);
             exit(0);
         }
