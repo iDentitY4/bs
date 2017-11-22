23d71d2a89dc1dac3fdda6573f73e31ccd08a92f
Manuel Ketterer <ps3udonym4@googlemail.com>
2017-11-22 13:45:35 +0100

    Added functionality for Praktikum 1

--- main.cpp
+++ main.cpp
@@ -5,24 +5,25 @@
 #include <signal.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
+#include <errno.h>
 #include <fcntl.h>
 #include <time.h>
 
 #define COMMAND_MAX_CHARS 512
 #define PARAM_MAX_SIZE 60
 
-void read_command(char *com, char **par)
+int read_command(char *com, char **par)
 {
     char dir [1024];
     fprintf(stdout, "%s > ", getcwd(dir, 1024));
-    
+
     //read user command
     char input [COMMAND_MAX_CHARS];
     fgets(input, COMMAND_MAX_CHARS, stdin);
 
     //remove newline
     input[strcspn(input, "\n")] = 0;
-    
+
     //first token is the command
     char * tok;
     if((tok = strtok(input, " ")) == NULL)
@@ -30,7 +31,7 @@ void read_command(char *com, char **par)
       return;
     }
     strcpy(com, tok);
-    
+
     par[0] = (char *) malloc(sizeof(char) * PARAM_MAX_SIZE);
     strcpy(par[0], tok);
 
@@ -44,23 +45,23 @@ void read_command(char *com, char **par)
     par[i] = (char *) malloc(1);
     par[i] = NULL;
 
-    return;
+    return i;
 }
 
 void handleQuit(int)
 {
     fprintf(stdout, "\nDo you really want to quit (y/n)?");
     fflush(stdout);
-    
+
     char buffer [8];
     fgets(buffer, 8, stdin);
-    
+
     if(buffer[0] == 'Y' || buffer[0] == 'y')
     {
         exit(0);
     }
     fflush(stdin);
-    
+
     return;
 }
 
@@ -71,34 +72,26 @@ void handleSegv(int)
     exit(2);
 }
 
-void log(char *filename, char *com, char **parameters)
+void log(FILE * logFile, char *com, char **parameters)
 {
-    FILE * f;
-    if((f = fopen(filename, "a")) == NULL)
-    {
-        fprintf(stderr, "error opening log file\n");
-        exit(2);
-    }
-
     time_t t;
     struct tm * timeinfo;
-    
+
     time(&t);
     timeinfo = localtime(&t);
-    
+
     //log command
     char time [80];
     strftime(time, 80, "%a %e %b %Y %T", timeinfo);
-    fprintf(f, "[%s]", time);
+    fprintf(logFile, "[%s]", time);
 
     for(int i = 0; parameters[i] != NULL; i++)
     {
-        fprintf(f," %s", parameters[i]);
+        fprintf(logFile," %s", parameters[i]);
     }
-    fprintf(f, "\n");
-    
-    fflush(f);
-    fclose(f);
+    fprintf(logFile, "\n");
+
+    fflush(logFile);
 }
 
 int main()
@@ -108,8 +101,8 @@ int main()
     char command[20];
     char * parameters[PARAM_MAX_SIZE];
     int logFd;
-    char logFileName [10] = "input.log";
-    
+    char logFileName [256] = "input.log";
+
     signal(SIGINT, handleQuit);
     signal(SIGSEGV, handleSegv);
 
@@ -120,11 +113,19 @@ int main()
       exit(2);
     }
 
+    //open log FILE
+    FILE * logFile;
+    if((logFile = fdopen(logFd, "a")) == NULL)
+    {
+        fprintf(stderr, "error opening log file\n");
+        exit(2);
+    }
+
     while(1)
     {
-        read_command(command, parameters);
+        int numParams = read_command(command, parameters);
 
-        log(logFileName, command, parameters);
+        log(logFile, command, parameters);
 
         //flush stream before forking to avoid stream errors
         fflush(stdin);
@@ -133,10 +134,16 @@ int main()
         //check for cd
         if(strcmp(command, "cd") == 0)
         {
-            //TODO
+            if(chdir(parameters[1]) < 0)
+            {
+                int errsv = errno;
+                fprintf(stderr, "can't change directory. error code: %d \n", errsv);
+            }
+
+            //don't fork on cd, no child process required
             continue;
         }
-        
+
         if((childPid = fork()) == -1)
         {
             fprintf(stderr, "can't fork\n");
@@ -144,12 +151,23 @@ int main()
         }
         else if(childPid == 0) //child
         {
-            execvp(command, parameters);
+            if(execvp(command, parameters) < 0)
+            {
+                //an error occured executing command
+                int errsv = errno;
+                fprintf(stderr, "error executing command. error code: %d\n", errsv);
+            }
             exit(0);
         }
         else // parent process
         {
-            waitpid(childPid, &status, WUNTRACED | WCONTINUED);
+            if(parameters[numParameters-1] != "&")
+            {
+                waitpid(childPid, &status, WUNTRACED | WCONTINUED);
+            }
         }
     }
+
+    //close log file
+    fclose(logFile);
 }
